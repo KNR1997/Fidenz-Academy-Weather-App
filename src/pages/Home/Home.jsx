da48d3ba36443991c3c86cb40fd55e7fad126fc7
KNR1997 <kethakaranasinghe@gmail.com>
2023-09-26 18:15:00 +0530

    Fidenz: Fetch data from cities.json file

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import cities from '../../assests/cities.json';
 
 function Home() {
+  const [jsonData, setJsonData] = useState([]);
+
+  useEffect(() =>{
+    setJsonData(cities.List);
+  }, []);
+
   return (
     <div>
-        <h2>Home</h2>
+      <h2>Home</h2>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
