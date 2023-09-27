855369dc0b8c54eccad3c953c5ab6343cd906eda
KNR1997 <kethakaranasinghe@gmail.com>
2023-09-27 11:53:34 +0530

    Fidenz: UI enhanced with toast notification

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,29 +1,50 @@
 import React, { useEffect, useState } from "react";
 import cities from "../../assests/cities.json";
 import "./home.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function Home() {
   const [jsonData, setJsonData] = useState([]);
   const [cityName, setCityName] = useState();
   const [city, setCity] = useState();
+  const [response, setResponse] = useState();
 
   useEffect(() => {
     setJsonData(cities.List);
   }, []);
 
-  const handleSearch = () => {
+  useEffect(() => {
+    checkValidCity(cityName);
+  }, [cityName]);
+
+  const handleSearch = async () => {
     if (checkValidCity(cityName)) {
       console.log(city);
       fetchData();
     } else {
       console.log("not a valid city");
+      notify();
     }
   };
 
-  const checkValidCity = (city) => {
+  const notify = () => {
+    toast.error("Invalid City Name", {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+    });
+  };
+
+  const checkValidCity = (cityName) => {
     let result = false;
     jsonData.forEach((object) => {
-      if (object.CityName.toLowerCase() === city.toLowerCase()) {
+      if (object.CityName.toLowerCase() === cityName.toLowerCase()) {
         setCity(object);
         result = true;
       }
@@ -44,14 +65,15 @@ function Home() {
       }
 
       const jsonData = await response.json();
+      setResponse(jsonData.list[0]);
       console.log(jsonData);
-
     } catch (err) {
       console.log(err);
     }
   };
 
-  console.log(jsonData);
+  console.log("cityName", cityName);
+  console.log("city", city);
 
   return (
     <div id="home">
@@ -66,7 +88,43 @@ function Home() {
             onChange={(e) => setCityName(e.target.value)}
           ></input>
           <button onClick={handleSearch}>Search</button>
+          <div>
+            <ToastContainer />
+          </div>
         </div>
+        {response && (
+          <div className="city">
+            <div className="top">
+              <div className="city-name">{response.name}</div>
+              <div className="top-divide">
+                <div className="divide-left">
+                  {response.weather[0].description}
+                </div>
+                <div className="divide-right">
+                  <h3 className="temparatue">{response.main.temp}</h3>
+                  <h4 className="sm">Temp Min: {response.main.temp_min}</h4>
+                  <h4 className="sm">Temp Max: {response.main.temp_max}</h4>
+                </div>
+              </div>
+            </div>
+            <div className="bottom">
+              <div>
+                <h4 className="sm">Pressure: {response.main.pressure}</h4>
+                <h4 className="sm">Humidity: {response.main.humidity}</h4>
+                <h4 className="sm">
+                  Visibility: {response.visibility / 1000}km
+                </h4>
+              </div>
+              <div>
+                {response.wind.speed}m/s {response.wind.deg} Degree
+              </div>
+              <div>
+                <h4 className="sm">Sunrise: {}</h4>
+                <h4 className="sm">Sunset: {}</h4>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
